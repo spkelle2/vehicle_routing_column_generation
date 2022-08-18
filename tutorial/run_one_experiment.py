0bf832bd8a0d632ca10dc475b8f8817c56cae428
Sean <seanpkelley94@gmail.com>
2022-08-17 20:46:46 -0700

    messed up run order

--- tutorial/run_one_experiment.py
+++ tutorial/run_one_experiment.py
@@ -19,19 +19,19 @@ def run_one_experiment(sln_root_pth: str, orders: str, input_pth, run_time=None)
     if not os.path.exists(sln_root_pth):
         os.mkdir(sln_root_pth)
 
-    # run exact method
-    exact_sln_pth = os.path.join(sln_root_pth, f'solution_{orders}_exact')
-    exact_vrp = ExactVRP(input_pth=input_pth, solution_pth=exact_sln_pth)
-    if run_time:
-        exact_vrp.parameters['max_solve_time'] = int(run_time)
-    exact_vrp.solve()
-
     # run heuristic methods
     heuristic_sln_pth = os.path.join(sln_root_pth, f'solution_{orders}_heuristic')
     heuristic_vrp = HeuristicVRP(input_pth=input_pth, solution_pth=heuristic_sln_pth)
     if run_time:
         heuristic_vrp.parameters['max_solve_time'] = int(run_time)
     heuristic_vrp.solve()
+        
+    # run exact method
+    exact_sln_pth = os.path.join(sln_root_pth, f'solution_{orders}_exact')
+    exact_vrp = ExactVRP(input_pth=input_pth, solution_pth=exact_sln_pth)
+    if run_time:
+        exact_vrp.parameters['max_solve_time'] = int(run_time)
+    exact_vrp.solve()
 
 
 if __name__ == '__main__':
